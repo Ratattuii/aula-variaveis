86ca6da18b8c541486478499530d952095993144
Ratatui <arthurselegarfritsch@gmail.com>
2023-08-31 17:08:56 -0300

    Conclusão do desafio

---
+++ desafio.js
@@ -0,0 +1,8 @@
+primeiroNumero = prompt("Insira um número")
+segundoNumero = prompt("Insira um segundo número")
+
+primeiroConvertido = Number(primeiroNumero)
+segundoConvertido = Number(segundoNumero)
+
+console.log("O primeiro número somado ao segundo número resulta em: ", primeiroConvertido + segundoConvertido);
+console.log("O primeiro número multiplicado pelo segundo número resulta em: ", primeiroConvertido * segundoConvertido);
